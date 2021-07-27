ce8e0af8700786cf7da00e1745ad0893e943d0e8
Arsalan Khalid <arslankhalid889@gmail.com>
2021-07-27 05:20:40 -0700

    updating todo

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.post("/todo", async(req, res) => {
     try {
         const { description } = req.body;
         const newTodo = await pool.query("INSERT INTO todo (description) VALUES($1) RETURNING *", [description]);
-        res.status(201).json(newTodo);
+        res.status(201).json(newTodo.rows);
     } catch (error) {
         console.log(error);
     }
@@ -41,6 +41,17 @@ app.get("/todo/:id", async(req, res) => {
         const { id } = req.params;
         const todo = await pool.query("SELECT * FROM todo WHERE todo_id=$1", [id])
         res.status(200).json(todo.rows);
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+app.patch("/todo/:id", async(req, res) => {
+    try {
+        const { id } = req.params;
+        const { desc } = req.body;
+        const todo = await pool.query("UPDATE todo SET description=$1 WHERE todo_id=$2", [desc, id])
+        res.status(204).json({message :"Updated Successfully"});
     } catch (error) {
         console.log(error);
     }
